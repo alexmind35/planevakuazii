1832556ee68cf6a65745a246e4fbfb8e6a7254f8
Alexander Mikhaylov <alexmind@yandex.ru>
2021-01-09 11:53:38 +0300

    Для приложения Orders созданы миграции

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import Customer, Executor
 
-admin.site.register(User)
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Executor)
+
--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     org_name = models.CharField(max_length=255)
     org_address = models.CharField(max_length=255)
     phone = models.CharField(max_length=11)
-    photo_executor = models.ImageField("Фото заказчика", upload_to="accounts/customer", default="", blank=True)
+    photo_customer = models.ImageField("Фото заказчика", upload_to="accounts/customer", default="", blank=True)
 
     def __str__(self):
         return "User: {}, org_name: {}, org_address: {}, phone: {}, photo_executor: {}".format(self.user, self.org_name,
