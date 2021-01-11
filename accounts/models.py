c1626895b7ca7c87b157a5761723817a58ab9a7b
Alexander Mikhaylov <alexmind@yandex.ru>
2021-01-11 15:59:57 +0300

    Добавлены подписи полей

--- accounts/models.py
+++ accounts/models.py
@@ -1,53 +1,61 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.hashers import make_password, identify_hasher
 from django.contrib.auth.models import AbstractUser
-from django.db.models import (EmailField, CharField, BooleanField, DateTimeField)
 from django.db import models
+from django.db.models import (EmailField, CharField)
 
 
 class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
     use_in_migrations = True
 
-    def create_user(self, email, password=None, name=None, full_name=None,
-                    is_active=True, is_staff=None, is_superuser=None):
-        """
-        Create and save a user with the given username, email, and password.
-        """
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
         if not email:
-            raise ValueError('Пользователь должен иметь и-мэйл адрес')
-        if not password:
-            raise ValueError('Пользователь должен ввести пароль')
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.is_staff = is_staff
-        user.is_superuser = is_superuser
-        user.is_active = is_active
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, name=None):
-        user = self.create_user(email, name=name, password=password,
-                                is_staff=True, is_superuser=True)
-        return user
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_staffuser(self, email, password=None, name=None):
-        user = self.create_user(email, name=name, password=password,
-                                is_staff=True, is_superuser=False)
-        return user
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    email = EmailField(unique=True, max_length=255)
-    name = CharField(max_length=255, blank=True, null=True)
-    photo = models.ImageField("Фото", upload_to="photo")
-    organization_name = models.CharField(max_length=255)
-    organization_address = models.CharField(max_length=255)
+    username = None
+    name = CharField("Имя пользователя", max_length=255)
+    phone = CharField("Телефон", max_length=11)
+    email = EmailField("Электронная почта", unique=True, max_length=255)
+    photo = models.ImageField("Фото", upload_to="photo", blank=True, null=True)
+    organization_name = models.CharField("Название организации", max_length=255)
+    organization_address = models.CharField("Адрес организации", max_length=255)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
 
     def __str__(self):
         return self.email
--- orders/models.py
+++ orders/models.py
@@ -6,8 +6,13 @@ User = get_user_model()
 
 class Service(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField("Название услуги", max_length=255)
     price = models.PositiveIntegerField("Цена")
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
 
     def __str__(self):
         return f'User: {self.user}, name: {self.name}, price: {self.price}'
@@ -15,7 +20,11 @@ class Service(models.Model):
 
 class Image(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    image_scheme = models.ImageField("Изображение схемы", upload_to="orders/service")
+    image_scheme = models.ImageField("Изображение план-схемы", upload_to="orders/service")
+
+    class Meta:
+        verbose_name = "План-схема"
+        verbose_name_plural = "План-схемы"
 
     def __str__(self):
         return "Service: {}, image_scheme: {}".format(self.service, self.image_scheme)
@@ -24,15 +33,20 @@ class Image(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    date_order = models.DateTimeField(auto_now_add=True)
-    time_order = models.TimeField(auto_now_add=True)
-    count_images = models.IntegerField()
+    date_order = models.DateTimeField("Дата создания заказа",auto_now_add=True)
+    time_order = models.TimeField("Время создания заказа", auto_now_add=True)
+    count_images = models.IntegerField("Количество план-схем")
     STATUS_ORDERS = [
         ('1', 'Новый'),
         ('2', 'Отменен'),
         ('3', 'Выполнен')
     ]
     status_order = models.CharField(choices=STATUS_ORDERS, default='1', max_length=1)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        ordering = ["-date_order"]
 
     def __str__(self):
         return "Customer: {}, service: {}, date_order: {}, time_order: {}, status_orders: {}, count_images: {}".format(
